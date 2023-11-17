c4abc2bbfe4a53a90c98dc60aa78ef8d546ef738
cinthiagodoi <cinthiagodoi@hotmail.com>
2023-11-17 18:36:35 +0000

    finish project

---
+++ README.md
@@ -0,0 +1,43 @@
+# Ecoleta
+
+View project to simulate bank transaction using composition API.
+
+### Prerequisites
+
+Before you begin, you will need to have the following tools installed on your machine: Git, Node.js., Vue, In addition, it is good to have an editor to work with code like VSCode
+
+### Installing
+
+🧭 Run Server(Backend)
+```
+# Clone this repository
+$ git clone git@github.com:cinthiagodoi/
+
+# Access the project folder in the / cmd terminal
+
+# Install the dependencies
+$ npm install
+
+# Run the application in development mode
+$ npm run start
+
+# The server will start at port:3000 - access http://localhost:3000
+
+```
+
+## Built With
+
+* [Vue](https://vuejs.org/)
+* [Javascript](https://developer.mozilla.org/pt-BR/docs/Web/JavaScript) 
+
+## Authors
+
+* **Cinthia Godoi** - [Contact](https://www.linkedin.com/in/cinthia-godoi/)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+## Acknowledgments
+
+* Project developed using Traversy Media tutorial
---
+++ src/App.vue
@@ -0,0 +1,86 @@
+<template>
+  <Header />
+  <div class="container">
+    <Balance :total="total"/>
+    <IncomeExpenses :income="+income" :expenses="+expenses"/>
+    <TransactionList :transactions="transactions" @transactionDeleted="handleDeleted"/>
+    <AddTransaction @transactionSubmitted="handleTransacionSubmitted" />
+  </div>
+</template>
+
+<script setup>
+  import Header from './components/Header.vue';
+  import Balance from './components/Balance.vue';
+  import IncomeExpenses from './components/IncomeExpenses.vue';
+  import TransactionList from './components/TransactionList.vue';
+  import AddTransaction from './components/AddTransaction.vue';
+
+  import { useToast } from 'vue-toastification';
+  import { ref, computed, onMounted } from 'vue';
+
+  const toast = useToast();
+
+  const transactions = ref([]);
+
+  onMounted(() => {
+    const savedTransactions = JSON.parse(localStorage.getItem('transactions'));
+    if(savedTransactions){
+      transactions.value = savedTransactions;
+    };
+  });
+
+  //get total
+  const total = computed(() => {
+    return transactions.value.reduce((acc, transaction) => {
+      return acc + transaction.amount
+    }, 0);
+  });
+
+  //get income
+  const income = computed(() => {
+    return transactions.value.filter((transaction) => transaction.amount > 0)
+    .reduce((acc, transaction) => {
+      return acc + transaction.amount
+    }, 0).toFixed(2);
+  });
+
+  //get expenses
+  const expenses = computed(() => {
+    return transactions.value.filter((transaction) => transaction.amount < 0)
+    .reduce((acc, transaction) => {
+      return acc + transaction.amount
+    }, 0).toFixed(2);
+  });
+
+  //add transaction
+  const handleTransacionSubmitted = (transactionData) => {
+    console.log(transactionData);
+    transactions.value.push({
+      id: generateUniqueId(),
+      text: transactionData.text,
+      amount: transactionData.amount
+    });
+    saveTransactionsToLocalStorage();
+    toast.success('Transaction added');
+    
+  };
+
+  //generate unique id
+  const generateUniqueId = () => {
+    return Math.floor(Math.random() * 1000);
+  }
+
+  //delete transaction
+  const handleDeleted = (id) => {
+    transactions.value = transactions.value.filter((transaction) => transaction.id != id );
+    saveTransactionsToLocalStorage();
+    toast.success('Transaction deleted');
+   
+  }
+
+  //save to localstoerage
+  const saveTransactionsToLocalStorage = () => {
+    localStorage.setItem('transactions', JSON.stringify(transactions.value));
+  }
+  
+</script>
